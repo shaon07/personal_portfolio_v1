b71c261537d07f3abc3199a2abf4b8522e9d3d86
SHAON <mdshamirulislam@SHAONs-MacBook-Air.local>
2024-02-26 23:27:36 +0600

    [add] recent companies

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,16 +2,19 @@ import { MacbookScroll } from "@/components/ui/macbook-scroll";
 import React from "react";
 import { useScroll, useTransform } from "framer-motion";
 import { GoogleGeminiEffectDemo } from "@/components/sub/GeminiEffect";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import { CardHoverEffectDemo } from "@/components/sub/CardHoverEffect";
+import { TextGenerateEffectDemo } from "@/components/sub/TextGenerateEffect";
 
 export default function About() {
-
-
   return (
     <div>
-      <MacbookScroll
-        src="/github-screen.png"
-      />
+      <MacbookScroll src="/github-screen.png" />
       <GoogleGeminiEffectDemo />
+      <div className="mt-16">
+        <TextGenerateEffectDemo />
+        <CardHoverEffectDemo />
+      </div>
     </div>
   );
 }
---
+++ components/sub/TextGenerateEffect.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { TextGenerateEffect } from "../ui/text-generate-effect";
+
+const words = `RECENT COMPANIES`;
+
+export function TextGenerateEffectDemo() {
+  return <TextGenerateEffect words={words} />;
+}
