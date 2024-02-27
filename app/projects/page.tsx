e37be3346cb2450bd9aae4bb14632023f53041d8
SHAON <mdshamirulislam@SHAONs-MacBook-Air.local>
2024-02-27 16:10:55 +0600

    [add] lamp design title

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,18 +1,12 @@
 import { HeroParallaxDemo } from "@/components/sub/HeroParallaxSkills";
+import { LampDemo } from "@/components/ui/lamp";
 import React from "react";
 
 export default function Projects() {
   return (
     <div className="relative bottom-16  z-[998]">
       <HeroParallaxDemo />
-      <h1 className="text-white">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore optio
-        nam ipsa rerum eos maxime, eveniet explicabo facere eligendi id ipsum
-        veritatis quaerat cumque magnam asperiores velit fugiat repudiandae
-        neque recusandae magni culpa sit rem. Illo vero eligendi placeat!
-        Reiciendis, fuga cumque quod facilis id obcaecati alias. Velit, libero.
-        Nam!
-      </h1>
+      <LampDemo />
     </div>
   );
 }
--- components/ui/hero-parallax.tsx
+++ components/ui/hero-parallax.tsx
@@ -105,12 +105,13 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0 ">
       <h1 className="text-2xl md:text-7xl font-bold text-white">
-        The Ultimate <br /> development studio
+      
+      . <br /> Development Cycle
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 text-neutral-200">
-        We build beautiful products with the latest technologies and frameworks.
-        We are a team of passionate developers and designers that love to build
-        amazing products.
+      Over 2 years of experience in designing and developing web applications
+on, focused primarily on the web, and components. Enjoy challenges, thrives in teams, is a hard worker and well
+communicator.
       </p>
     </div>
   );
